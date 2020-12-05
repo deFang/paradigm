1c45d60be262a3bf17d3b172c99dcae9ed8b5b4d
daniel <runiu_fang@163.com>
2020-12-05 13:46:33 +0800

    Updates

---
+++ precache-manifest.32f9203cf157b9972d5120e5e67ad5d4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e628e32570977edccdbb4614c27bc759",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db83abef7da369146416",
+    "url": "./static/css/main.1f11ae23.chunk.css"
+  },
+  {
+    "revision": "84a5c8f1ad9f46fb049a",
+    "url": "./static/js/2.c388a33f.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "./static/js/2.c388a33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db83abef7da369146416",
+    "url": "./static/js/main.a3814377.chunk.js"
+  },
+  {
+    "revision": "0ad5f22681f41f2e8516",
+    "url": "./static/js/runtime-main.d85e37e0.js"
+  },
+  {
+    "revision": "3616c5a20c8128561528f6151c823210",
+    "url": "./static/media/Parapara.3616c5a2.svg"
+  },
+  {
+    "revision": "027b09e3f1bc0f24d78047af072bcd8c",
+    "url": "./static/media/Parapara_single.027b09e3.svg"
+  },
+  {
+    "revision": "f188158b6491ccd9f1234654f2207ffe",
+    "url": "./static/media/bag-holder.f188158b.svg"
+  },
+  {
+    "revision": "309a7df740bb204a094d606d645ea974",
+    "url": "./static/media/dao.309a7df7.svg"
+  },
+  {
+    "revision": "211b9b1a8b998f1eef24d22296c53734",
+    "url": "./static/media/howto.211b9b1a.png"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "./static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "2a6292559219b2e6844b47eaf10bca7c",
+    "url": "./static/media/roadmap.2a629255.svg"
+  },
+  {
+    "revision": "48a6b4cbcb180537edc24829c6b618aa",
+    "url": "./static/media/roadmap.48a6b4cb.png"
+  },
+  {
+    "revision": "4aaeac08be574e5574ad17d2d99351c0",
+    "url": "./static/media/whatispara.4aaeac08.png"
+  }
+]);
